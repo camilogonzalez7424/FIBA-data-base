af20f35c15dddc0c9de8ed269a2c18f45b4722a9
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2021-11-02 21:30:44 -0500

    app test

--- test/model/AppTest.java
+++ test/model/AppTest.java
@@ -3,23 +3,29 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AppTest {
 
 	App app;
-
+/*
 	private void setupScenery1() throws FileNotFoundException, IOException { 
 		app = new App();
 		for (int i = 0; i < 4; i++) {
 			Player temp = new Player("name"+i, i, "Delfines", i, 5, 6, 8+i, i);
-			app.addPlayer(temp);
+			//app.addPlayer(temp);
 		}
-	}
+	}*/
 
+	private void setupScenery1() throws IOException {
+		app = new App();
+		Player temp = new Player("El bicho", 19, "Tapitas", 3, 4, 5, 6, 7);
+
+		app.addPlayer(temp);
+	}
 
 	@Test
 	public void testApp() throws Exception {
@@ -29,11 +35,19 @@ public class AppTest {
 	@Test
 	public void testAddPlayer() throws Exception {
 		setupScenery1();
+
+		assertEquals("El bicho,19,Tapitas,3,4,5,6,7",app.getPlayers().toString().replaceAll("\\[|\\]", ""));
 	}
 
 	@Test
 	public void testClean() throws Exception {
 		setupScenery1();
+
+		app = new App();
+
+		assertEquals("", app.getPlayers().toString().replaceAll("\\[|\\]", ""));
+
+		//Cleal all
 	}
 
 }
